a4b0dd740d367bbd6929075695a2e18b56bd79f4
Daniel Biedermann <d.biedermann@dipf.de>
2023-03-16 09:22:26 +0100

    batch logging

--- lib/services/logging_service.dart
+++ lib/services/logging_service.dart
@@ -1,9 +1,17 @@
+import 'dart:async';
+
 import 'package:prompt/services/data_service.dart';
 
 class LoggingService {
   List<Map<String, dynamic>> logs = [];
   DataService _dataService;
-  LoggingService(this._dataService);
+  final int batchDuration;
+  late Timer _batchTimer;
+  LoggingService(this._dataService, {this.batchDuration = 30}) {
+    _batchTimer = Timer.periodic(Duration(seconds: batchDuration), (_) {
+      _sendBatch();
+    });
+  }
 
   String getTimestamp() {
     return DateTime.now().toLocal().toIso8601String();
@@ -17,7 +25,7 @@ class LoggingService {
       "data": data ?? ""
     };
     logs.add(event);
-    _dataService.logData(event);
+    // _dataService.logData(event);
   }
 
   logError(String eventName, {String? data = ""}) {
@@ -29,6 +37,17 @@ class LoggingService {
       "data": data ?? ""
     };
     logs.add(event);
-    _dataService.logData(event);
+    // _dataService.logData(event);
+  }
+
+  void _sendBatch() async {
+    if (logs.isEmpty) return;
+    final batch = logs.toList();
+    logs.clear();
+    _dataService.sendLogs(batch);
+  }
+
+  void dispose() {
+    _batchTimer.cancel();
   }
 }
