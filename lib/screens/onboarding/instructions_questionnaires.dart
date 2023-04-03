5339203c2ba76fdab6f40d5d41788efddffc04c8
jasminbreitwieser <jasmin_breitwieser@web.de>
2023-04-03 11:48:20 +0200

    Update instructions_questionnaires.dart
    added some styling

--- lib/screens/onboarding/instructions_questionnaires.dart
+++ lib/screens/onboarding/instructions_questionnaires.dart
@@ -17,13 +17,45 @@ class InstructionsQuestionnaires extends StatelessWidget {
       //         alignment: Alignment.bottomCenter)),
       child: ListView(
         children: [
-          Text(S.of(context).instructionsQuestionnaires_p1),
-          UIHelper.verticalSpaceSmall,
+          Text(
+            S.of(context).instructionsQuestionnaires_p1,
+            style: Theme.of(context).textTheme.titleMedium,
+            textAlign: TextAlign.center,
+          ),
+          UIHelper.verticalSpaceMedium,
           Text(S.of(context).instructionsQuestionnaires_p2),
+          UIHelper.verticalSpaceSmall,
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Theme.of(context).dialogBackgroundColor.withOpacity(0.9),
+              border: Border.all(color: Color(0xFF000000), width: 1),
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            ),
+            child: Text(
+              S.of(context).instructionsQuestionnaires_p2b,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          // Text(S.of(context).instructionsQuestionnaires_p2b),
           UIHelper.verticalSpaceSmall,
           MarkdownBody(data: S.of(context).instructionsQuestionnaires_p3),
           UIHelper.verticalSpaceSmall,
-          MarkdownBody(data: S.of(context).instructionsQuestionnaires_p4),
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Theme.of(context).dialogBackgroundColor.withOpacity(0.9),
+              border: Border.all(color: Color(0xFF000000), width: 1),
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+            ),
+            child: Text(
+              S.of(context).instructionsQuestionnaires_p4,
+              textAlign: TextAlign.left,
+            ),
+          ),
+          // MarkdownBody(data: S.of(context).instructionsQuestionnaires_p4),
           UIHelper.verticalSpaceSmall,
           MarkdownBody(data: S.of(context).instructionsQuestionnaires_p5),
         ],
